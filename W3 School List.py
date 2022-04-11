91333db04c452266fd0ea1a73315565c1a058640
M-612 <101103673+M-612@users.noreply.github.com>
2022-04-11 14:02:28 +0530

    Add files via upload

---
+++ W3 School List.py
@@ -0,0 +1,68 @@
+vegetables= ['brinjal', 'chilli', 'lemon']
+vegetables.append("Coriander")
+print (vegetables)
+
+a = ["brinjal", "chilli", "lemon"]
+b = ["tennis", "Football", "Ludo"]
+a.append(b)
+
+vegetables = ['brinjal', 'chilli', 'lemon', 'coriander']
+vegetables.clear()
+print (vegetables)
+
+vegetables = ['brinjal', 'chilli', 'lemon', 'coriander']
+x = vegetables.copy()
+print (vegetables)
+
+vegetables =['brinjal', 'chilli', 'lemon']
+x = vegetables.count("lemon")
+print (vegetables)
+
+points = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+x = points.count(9)
+print (points)
+
+vegetables =["brinjal", "chilli", "lemon"]
+games =["tennis", "Football", "Ludo"]
+vegetables.extend(games)
+print (vegetables)
+
+vegetables = ["brinjal", "chilli", "lemon"]
+points = (1, 4, 5, 9)
+vegetables.extend(points)
+print (vegetables)
+
+vegetables =["brinjal", "chilli", "lemon"]
+x = vegetables.index("lemon")
+print (vegetables)
+
+vegetables = [4, 55, 64, 32, 16, 32]
+x = vegetables.index(32)
+print (vegetables)
+
+vegetables = ["brinjal", "chilli", "lemon"]
+vegetables.insert(1, "coriander")
+print (vegetables)
+
+vegetables = ["brinjal", "chilli", "lemon"]
+vegetables.pop(1)
+print (vegetables)
+
+vegetables = ["brinjal", "chilli", "lemon"]
+x = vegetables.pop(1)
+print (vegetables)
+
+vegetables = ["brinjal", "chilli", "lemon"]
+vegetables.remove("chilli")
+print (vegetables)
+
+vegetables = ["brinjal", "chilli", "lemon"]
+vegetables.reverse()
+print (vegetables)
+
+games = ["tennis", "Football", "Ludo"]
+games.sort()
+print (games)
+
+
+
